236b0bfdf325b85983dbe38af1643cc0b97050d2
Osurse <matyaza@gmail.com>
2022-11-10 14:58:52 -0600

    actualizacion funcional 3

--- script.js
+++ script.js
@@ -1,31 +1,55 @@
-
-        let imagenes = []
+        let imagenes=[]
         let selecciones = []
+        let fallos=0
+        let acierto=0;
+        let error=document.getElementById('errores');
+        let final=document.getElementById('final');
+
         var espera= document.getElementsByClassName("area-tarjeta");
         Gencartas()
 
-        function Cargarimg() {
-            imagenes = [
+        function Cargarimg(foto) {
+            foto = [
                 '<img src="img/prueba.jpg" heigh:"90%" width="80%">',
                 '<img src="img/prueba2.jpg" heigh:"90%" width="80%">',
                 '<img src="img/prueba3.jpg" heigh:"90%" width="80%">',
-                '<i class="fas fa-star"></i>',
-                '<i class="far fa-star"></i>',
-                '<i class="fas fa-star-of-life"></i>',
-                '<i class="fas fa-star-and-crescent"></i>',
-                '<i class="fab fa-old-republic"></i>',
-                '<i class="fab fa-galactic-republic"></i>',
-                '<i class="fas fa-sun"></i>',
-                '<i class="fas fa-stroopwafel"></i>',
-                '<i class="fas fa-dice"></i>',
-                '<i class="fas fa-chess-knight"></i>',
-                '<i class="fas fa-chess"></i>',
-                '<i class="fas fa-dice-d20"></i>',
+                '<img src="img/prueba4.jpg" heigh:"90%" width="80%">',
+                '<img src="img/prueba5.jpg" heigh:"90%" width="80%">',
+                '<img src="img/prueba6.jpg" heigh:"90%" width="80%">',
+                '<img src="img/prueba7.jpg" heigh:"90%" width="80%">',
+                '<img src="img/prueba8.jpg" heigh:"90%" width="80%">',
+                '<img src="img/prueba9.jpg" heigh:"90%" width="80%">',
+                '<img src="img/prueba10.jpg" heigh:"90%" width="80%">',
+                '<img src="img/prueba11.jpg" heigh:"90%" width="80%">',
+                '<img src="img/prueba12.jpg" heigh:"90%" width="80%">',
+                '<img src="img/prueba13.jpg" heigh:"90%" width="80%">',
+                '<img src="img/prueba14.jpg" heigh:"90%" width="80%">',
+                '<img src="img/prueba15.jpg" heigh:"90%" width="80%">',
+                '<img src="img/prueba16.jpg" heigh:"90%" width="80%">',
+                '<img src="img/prueba17.jpg" heigh:"90%" width="80%">',
+                '<img src="img/prueba18.jpg" heigh:"90%" width="80%">',
+                '<img src="img/prueba19.jpg" heigh:"90%" width="80%">',
+                '<img src="img/prueba20.jpg" heigh:"90%" width="80%">',
+                '<img src="img/prueba21.jpg" heigh:"90%" width="80%">',
+                '<img src="img/prueba22.jpg" heigh:"90%" width="80%">',
+                '<img src="img/prueba23.jpg" heigh:"90%" width="80%">',
+                '<img src="img/prueba24.jpg" heigh:"90%" width="80%">',
+                '<img src="img/prueba25.jpg" heigh:"90%" width="80%">',
+                '<img src="img/prueba26.jpg" heigh:"90%" width="80%">',
+                '<img src="img/prueba27.jpg" heigh:"90%" width="80%">',
+                '<img src="img/prueba28.jpg" heigh:"90%" width="80%">',
+                '<img src="img/prueba29.jpg" heigh:"90%" width="80%">',
+                '<img src="img/prueba30.jpg" heigh:"90%" width="80%">',
+                '<img src="img/prueba31.jpg" heigh:"90%" width="80%">',
+                '<img src="img/prueba32.jpg" heigh:"90%" width="80%">',
             ]
+
+           imagenes=foto.sort(()=> Math.random()-0.5);
         }
 
         function Gencartas() {
             Cargarimg()
+            fallos=0;
             selecciones = []
             let tablero = document.getElementById("tablero")
             let tarjetas = []
@@ -48,6 +72,8 @@
             }
             tarjetas.sort(() => Math.random() - 0.5)
             tablero.innerHTML = tarjetas.join(" ")
+            error.innerHTML='<h4>Intentos: '+fallos+'</h4>'
+            final.innerHTML=''
         }
 
         function seleccionarTarjeta(i) {
@@ -61,6 +87,8 @@
 
                 ValidarSelecciones(selecciones)
                 selecciones = []
+                fallos++;
+                error.innerHTML='<h4>Intentos: '+fallos+'</h4>'
                 
 
             }
@@ -87,6 +115,10 @@
                 }else{
                     trasera1.style.background = "green"
                     trasera2.style.background = "green"
+                    acierto++;
+                    if(acierto==8){
+                        final.innerHTML='<br> <h4 class="text-success">Ganaste manco</h4> '
+                    }
                 }
                 tablero.setAttribute('style','');
             }, 1000);
